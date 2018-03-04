8285a003bcd58da77fad4948a64fee5f0a26e23e
tianyu1994 <tianyu_tianyu1994@163.com>
2018-03-04 22:13:57 +0800

    Update head.py

--- head.py
+++ head.py
@@ -1,26 +1,11 @@
 '''
 this python file for sharing
 
-the PairingGroup:group,
-pulblic key:g,
-CSP's secret key:sk
+the PairingGroup:group;
+pulblic key:g;
 '''
 
 from charm.toolbox.pairinggroup import PairingGroup, ZR, G1, GT, pair
 
 group = PairingGroup('SS512')
 g = group.random(G1)
-sk = group.random(ZR)#CSP secret key
-
-# some params in tian's scheme
-tian_r = group.random(ZR)
-
-# read blocksize for one time
-blockSize = 128*1000#bytes
-
-
-
-segmentNum = 200
-blockNumber= 50000          #62500
-trialsNumber = 20
-
